ef9b1a176618ece5d129136b13c02da4e9139ae1
Josiah <Josiahp4@illinois.edu>
2025-11-17 12:15:46 -0600

    - Finished filter-listings with arbitrary filter parameters and database column names

--- Project/next-pages-template/pages/api/filter-listings.js
+++ Project/next-pages-template/pages/api/filter-listings.js
@@ -5,7 +5,21 @@ export default async function filterListings(req, res) {
     if (req.method !== "GET") {
         res.status(405).json({error: 'Method not allowed'});
     }
-    
+
     // get info from req (assuming how it is given)
-    const {} = req.params;
-    
+    const {textbooks, electronics, clothing, vehicles, fitness, minPrice, maxPrice} = req.params;
+
+    try {
+        // query assuming what the database names will be 
+        await pool.query(
+        'SELECT product_id FROM products WHERE f_textbooks = $1 AND f_electronics = $2 AND f_clothing = $3 AND f_vehicles = $4 AND f_fitness = $5 AND f_price <= $6 AND f_price >= $7', [textbooks, electronics, clothing, vehicles, fitness, minPrice, maxPrice]
+        );
+
+        // success
+        res.status(200).json("Filtered search updated")
+        
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({error: 'An unexpected error occurred while filtering searches'})
+    }
+}
